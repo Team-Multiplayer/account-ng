0fc4f9f975148f688ceb25af84f15a097a27258d
LucasVillarim <villarim94@gmail.com>
2021-02-17 15:03:16 -0300

    corrigindo dashboard

---
+++ src/app/pages/area-logada/dashboard/dashboard.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DashboardService {
+
+  constructor(
+    private router: Router,
+    private httpClient: HttpClient
+    ) { }
+
+
+}
+
