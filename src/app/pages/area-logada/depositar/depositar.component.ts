b84a7898e94e6592de5e1071cdc2447103a573a1
Danilo Elias <daniloselias@gmail.com>
2021-02-18 14:43:34 -0300

    Adicionar retorno das validações dos fomulários

--- src/app/pages/area-logada/depositar/depositar.component.ts
+++ src/app/pages/area-logada/depositar/depositar.component.ts
@@ -32,10 +32,10 @@ export class DepositarComponent implements OnInit {
   isOpen: boolean = false;
 
   userId = JSON.parse(localStorage.getItem('usuario'));
-  lancamentoForm = this.formBuilder.group({
+  lancamentoForm: FormGroup = this.formBuilder.group({
     idContaUsuario:     ['', Validators.required],
     descricao:          ['', Validators.required],
-    valor:              ['', Validators.required],
+    valor:              ['', [Validators.required, Validators.min(0)]],
     tipo:               ['CREDITO'],
     categoria:          [1],
   })
@@ -60,28 +60,17 @@ export class DepositarComponent implements OnInit {
 
   fazerDeposito() {
 
+    if (!this.lancamentoForm.valid) {
+      this.validarCamposDoFormulario(this.lancamentoForm);
+      this.focarNoPrimeiroInputInvalido(this.lancamentoForm);
+      return;
+    }
+
     this.lancamentoService.novoLancamento(this.lancamentoForm.value)
     .subscribe(
       response => this.onSuccess(response),
       error => this.onError(error)
       );
-    }
-
-    private validarCamposDoFormulario(form: FormGroup) {
-    Object.keys(form.controls).forEach(field => {
-      const control = form.get(field) as FormControl;
-      control.markAsTouched();
-    });
-  }
-
-  private focarNoPrimeiroInputInvalido(form: FormGroup) {
-    for (let control of Object.keys(form.controls)) {
-      if (form.controls[control].invalid) {
-        const input = `${control}Input` as keyof DepositarComponent;
-        (this[input] as ElementRef).nativeElement.focus();
-        break;
-      }
-    }
   }
 
   onSuccess(response) {
@@ -98,6 +87,30 @@ export class DepositarComponent implements OnInit {
 
   resetForm() {
     this.lancamentoForm.reset();
+  }
+
+  private validarCamposDoFormulario(form: FormGroup) {
+    Object.keys(form.controls).forEach(field => {
+      const control = form.get(field) as FormControl;
+      control.markAsTouched();
+    });
+  }
+
+  private focarNoPrimeiroInputInvalido(form: FormGroup) {
+    for (let control of Object.keys(form.controls)) {
+      if (form.controls[control].invalid) {
+        const input = `${control}Input` as keyof LoginComponent;
+        (this[input] as ElementRef).nativeElement.focus();
+        break;
+      }
+    }
+  }
+
+  exibeErro(nomeControle: string) {
+    if (!this.lancamentoForm.controls[nomeControle]) {
+      return false;
+    }
+    return this.lancamentoForm.controls[nomeControle].invalid && this.lancamentoForm.controls[nomeControle].touched;
   }
 
 }
--- src/app/pages/area-logada/pagar/pagar.component.ts
+++ src/app/pages/area-logada/pagar/pagar.component.ts
@@ -1,6 +1,6 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { LancamentoService } from 'src/app/service/lancamento/lancamento.service';
 
 @Component({
@@ -30,16 +30,17 @@ export class PagarComponent implements OnInit {
 
   isOpen: boolean = false;
 
-  lancamentoForm = this.formBuilder.group({
+  lancamentoForm: FormGroup = this.formBuilder.group({
     idContaUsuario:     ['', Validators.required],
     descricao:          ['', Validators.required],
-    valor:              ['', Validators.required],
+    valor:              ['', [Validators.required, Validators.min(0)]],
     tipo:               ['DEBITO'],
     categoria:          [1]
   })
 
   contaCorrente;
   contaCredito;
+  erroLancamento = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -52,6 +53,13 @@ export class PagarComponent implements OnInit {
   }
 
   sendLancamento() {
+
+    if (!this.lancamentoForm.valid) {
+      this.validarCamposDoFormulario(this.lancamentoForm);
+      this.focarNoPrimeiroInputInvalido(this.lancamentoForm);
+      return;
+    }
+
     this.lancamentoService.novoLancamento(this.lancamentoForm.value)
     .subscribe(
       response => this.onSuccess(response),
@@ -64,6 +72,7 @@ export class PagarComponent implements OnInit {
   }
 
   onError(error) {
+    this.erroLancamento = true;
     console.log(error);
   }
 
@@ -73,6 +82,30 @@ export class PagarComponent implements OnInit {
 
   resetForm() {
     this.lancamentoForm.reset();
+  }
+
+  private validarCamposDoFormulario(form: FormGroup) {
+    Object.keys(form.controls).forEach(field => {
+      const control = form.get(field) as FormControl;
+      control.markAsTouched();
+    });
+  }
+
+  private focarNoPrimeiroInputInvalido(form: FormGroup) {
+    for (let control of Object.keys(form.controls)) {
+      if (form.controls[control].invalid) {
+        const input = `${control}Input` as keyof PagarComponent;
+        (this[input] as ElementRef).nativeElement.focus();
+        break;
+      }
+    }
+  }
+
+  exibeErro(nomeControle: string) {
+    if (!this.lancamentoForm.controls[nomeControle]) {
+      return false;
+    }
+    return this.lancamentoForm.controls[nomeControle].invalid && this.lancamentoForm.controls[nomeControle].touched;
   }
 
 }
